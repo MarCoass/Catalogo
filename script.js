3f4965230a737f11f93f97e9d472930493190610
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2023-02-11 01:53:17 -0300

    Agregada comprobacion de stock
    solo muestra los que tengan disponibilidad

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const contenedor = document.getElementById("cuerpo");
 let arrayNodos = [];
 for (i = 0; i < array.length; i++) {
-    let nombreEncode = encodeURIComponent("Hola! Me interesa el set "+ array[i]["Descripcion"] + " con forma " + array[i]["Forma"])
-    let urlMensaje = "https://wa.me/542994677550?text=" + nombreEncode
+  if(array[i]["Disponible"]=='si'){
+
+  
+  let nombreEncode = encodeURIComponent(
+    "Hola! Me interesa el set " +
+      array[i]["Descripcion"] +
+      " con forma " +
+      array[i]["Forma"]
+  );
+  let urlMensaje = "https://wa.me/542994677550?text=" + nombreEncode;
   //crea una tarjeta por cada nodo
   let nodo = document.createElement("div");
   let card =
     '<div class="card m-2" style="width: 18rem">' +
-    '<img src="http://drive.google.com/uc?export=view&id='+array[i]["FileId"]+'" class="card-img-top" alt="..." />' +
+    '<img src="http://drive.google.com/uc?export=view&id=' +
+    array[i]["FileId"] +
+    '" class="card-img-top" alt="..." />' +
     '<div class="card-body">' +
-    '<h5 class="card-title">'+array[i]["Descripcion"]+'</h5>' +
+    '<h5 class="card-title">' +
+    array[i]["Descripcion"] +
+    "</h5>" +
     "</div>" +
     '<ul class="list-group list-group-flush">' +
     '<li class="list-group-item">Precio: $' +
@@ -22,10 +34,12 @@ for (i = 0; i < array.length; i++) {
     array[i]["Forma"] +
     "</li>" +
     "</ul> " +
-    '<div class="d-grid gap-2 col-6 mx-auto"><a href="'+urlMensaje+'" class="btn btn-sm btn-outline-rosa"> <i class="bi bi-whatsapp" style="font-size: 1rem; " ></i>  Comprar</a></div>' +
-   "</div>";
-  nodo.innerHTML= card;
+    '<div class="d-grid gap-2 col-6 mx-auto"><a href="' +
+    urlMensaje +
+    '" class="btn btn-sm btn-outline-rosa"> <i class="bi bi-whatsapp" style="font-size: 1rem; " ></i>  Comprar</a></div>' +
+    "</div>";
+  nodo.innerHTML = card;
   arrayNodos.push(card);
-  contenedor.appendChild(nodo);
+  contenedor.appendChild(nodo);} 
 }
 console.log(arrayNodos);
